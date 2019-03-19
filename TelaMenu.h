085a8e43a0819e08602a1516c6a08de7d23911a3
Josue10599 <josue10599@gmail.com>
2019-03-19 11:27:06 -0300

    Adição TelaMenu.h

---
+++ TelaMenu.h
@@ -0,0 +1,20 @@
+/*
+ * Cabeçalho TelaInfo.h
+ *
+ * Autor: Josue Lopes
+ * 
+ * Função: Definir os componentes na tela de menu.
+ * 
+ */
+ 
+#ifndef TELA_MENU_H
+  #define TELA_MENU_H
+
+// Configura o id da página menu.
+  #define MENU 0
+
+// Configura o número dos componentes da tela menu.
+  #define B_INFO 1
+  #define B_RELA 2
+
+#endif //TELA_MENU_H
